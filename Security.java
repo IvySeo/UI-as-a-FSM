4c160aaae7b6e8d61903ad1fac84fb3cf8053607
Vachia Thoj <47841533+ViseROM@users.noreply.github.com>
2019-04-09 16:20:18 -0500

    Add files via upload

--- Security.java
+++ Security.java
@@ -1,21 +1,19 @@
 import java.util.*;
-import javax.swing.LookAndFeel;
 import java.text.*;
 import java.io.*;
-public class Security implements Serializable{
-	
-	private Warehouse warehouse;
-	
-	public boolean validateClerk(String userID)
-	{
-		if (warehouse.instance().searchClient(userID) != null)
-		{
-			return true;
-		}
-		else
-			return false;
-		
-	
-	}
+
+public class Security{
+    private static Warehouse warehouse;
+    
+    public Security(){
+        warehouse = Warehouse.instance();
+    }
+    
+    public boolean validateClerk(String userID){
+        if (warehouse.searchClient(userID) != null){
+            return true;
+        }else
+            return false;
+    }
 }
 
